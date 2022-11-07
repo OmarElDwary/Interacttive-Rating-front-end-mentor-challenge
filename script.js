c45ae4b02d2fb1c0091992789989356cc0f59fde
OmarElDwary <105021882+OmarElDwary@users.noreply.github.com>
2022-11-07 16:54:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const mainContainer = document.querySelector('.main-container')
+const thanksContainer = document.querySelector('.thank-you')
+const submitButton = document.getElementById("submit")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn")
+
+submitButton.addEventListener('click', () =>{
+    thanksContainer.classList.remove("hidden")
+    mainContainer.classList.add("hidden")
+})
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    })
+}) 
